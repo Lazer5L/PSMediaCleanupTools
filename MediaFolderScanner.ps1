20c4dfd42c06a0030eeb7d5d08a7148be3e84902
Lazer5L <brenteri@gmail.com>
2017-04-01 23:27:45 -0500

    Cleaned up Code
    Removed testing variables. Elimated file checks as this is only for
    folders.

--- MediaFolderScanner.ps1
+++ MediaFolderScanner.ps1
@@ -1,15 +1,16 @@
 ﻿$TVSource = "\\192.168.1.53\Multimedia\TV Shows" # Location of all TV Shows
+$OutputFile = (Join-Path $TVSource "IncorrectFolders.csv.")
 $incorrectFolder = @()
-$incorrectFiles = @()
 
 $TVFolders = Get-ChildItem -path $TVSource -Attributes Directory | Sort-Object # Get all show folder names
 Foreach ($ShowFolder in $TVFolders) {
     $SeasonFolder = Get-ChildItem -path $ShowFolder.FullName -Attributes Directory | Sort-Object # Get all season folder names
     $i=0
+    Clear-Host
+    $ShowFolder.FullName
     foreach ($Season in $SeasonFolder) {
         $i++
-        $Season.FullName
-        $i
+        write-host
 	    If (!($Season.FullName -match(“ [0-9][0-9]")) -and !($Season.FullName -match(“Special"))) { # If Season folder does NOT have two digits and is NOT Specials
 		    If ($Season.FullName -match(“Extra")) { # E comes before S so catch out of order structure
                 $NewName = "Specials" # Purposed New name for Folder
@@ -21,9 +22,9 @@ Foreach ($ShowFolder in $TVFolders) {
             $Loop = 0
             while ($Loop -eq 0) {
                 $PurposedName = (Join-Path $ShowFolder.FullName $NewName)
-                $PurposedName
                 if (Test-path $PurposedName) { # Check if Purposed name exists
-                    Read-host -prompt "$NewName - Duplicate folder Found! Merging folders. Press any key to continue"
+                    $Season.FullName
+                    Read-host -prompt "$NewName - Duplicate folder Found! Press any key to Merge Folders"
                     Get-ChildItem $Season.FullName | Move-Item -Destination $PurposedName -Confirm # Move all contents to folder we are keeping
                     if($Season.GetFiles().Count -eq 0) { # Is this folder empty
                         Remove-Item $Season.FullName # Delete duplicate
@@ -33,11 +34,11 @@ Foreach ($ShowFolder in $TVFolders) {
                 else { # Purposed name does not exist
                     write-host $Season.FullName
                     write-host "Change to: $PurposedName"
-                    write-host "[ Yes ]   [ No ]   [ Custom Folder Name ]   [ Fix Folder Counter ]"
+                    write-host "[ Yes ]   [ No ]   [ Custom Folder Name ]   [ Fix Folder Counter ($i) ]"
                     $MakeChange = read-host -prompt "(Y:N:C:F)"
                     switch($MakeChange) {
                         "Y" { # Perform Rename if user agrees
-                            rename-item -path $Season.FullName -NewName $NewName -confirm 
+                            rename-item -path $Season.FullName -NewName $NewName 
                             $Loop++    
                         } 
                         "N" { # Add path to output file
@@ -46,25 +47,17 @@ Foreach ($ShowFolder in $TVFolders) {
                             $loop++
                         }
                         "c" { # Prompt for custom folder name
-                            $NewName = read-host -prompt "Input new folder Name: "
+                            $NewName = read-host -prompt "Input new folder Name"
                         }
                         "f" { # Prompt for $i fix and generate new folder name
-                            [int]$i = read-host -prompt "What number should this be?:"
+                            [int]$i = read-host -prompt "What number should this be? ($i)"
                             $NewName = "Season {0:d2}" -f $i # Purposed New name for Folder
                         }
                     } # Switch $MakeChange
                 } # Else 
             } # While $Loop
 	    } # Season Check
-	    <#$Files = Get-ChildItem -path $Season.FullName -Attributes !Directory | Sort-Object # Get all file names
-	    Foreach ($file in $Files) {
-		    If (!($file.FullName -match(“s[0-9][0-9]e[0-9][0-9]"))) { # If episode does NOT have S99E99
-			    $IncorrectFiles += $file
-		    }
-        }#>
     }
 }
-$IncorrectFolder | select fullname | Export-csv “\\192.168.1.53\Multimedia\ProblemFolders.csv” -force # Output array to a file
-$IncorrectFiles | select fullname | Export-csv “\\192.168.1.53\Multimedia\ProblemFiles.csv” -force # Output array to a file
+$IncorrectFolder | select fullname | Export-csv $OutputFile -force # Output array to a file
 $IncorrectFolder = $Null
-$IncorrectFiles = $Null
